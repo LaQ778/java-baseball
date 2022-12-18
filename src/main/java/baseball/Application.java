32818455b69a61e9a2dcffae871b5f2337d1cfdb
LaQ778 <ddalaq778@gmail.com>
2022-12-19 04:29:10 +0900

    feat: 컴퓨터의 정답 숫자 생성 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,4 +1,6 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
 
 public class Application {
     public static void playGame() {
@@ -8,7 +10,18 @@ public class Application {
     public static void printStartMsg() {
         System.out.println("숫자 야구 게임을 실행합니다.");
     }
-    
+
+    public static List<Integer> makeComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         playGame();
--- src/test/java/baseball/UnitTest.java
+++ src/test/java/baseball/UnitTest.java
@@ -1,18 +1,34 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-public class UnitTest extends NsTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class UnitTest extends NsTest {
     @Test
+    @DisplayName("시작 테스트")
     void 시작_테스트() {
         Application.playGame();
         assertThat(output()).contains("숫자 야구 게임을 실행합니다.");
     }
+
+    @RepeatedTest(3)
+    @DisplayName("랜덤 숫자 생성 테스트")
+    void 랜덤_숫자_생성_테스트() {
+        List<Integer> computer = Application.makeComputerNumber();
+        System.out.println(computer);
+        assertEquals(computer.size(), 3);
+    }
+
     @Override
     protected void runMain() {
 
